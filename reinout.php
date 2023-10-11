806c0f9156a0b4e2d5c118c89a312b0c096542db
Reinout <94532201+ReinoutvdG@users.noreply.github.com>
2023-10-11 10:05:51 +0200

    standard css reinout

---
+++ reinout.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <nav>
+            
+        </nav>
+    </header>
+    <main>
+        <div class="wrapper">
+            <h1>Deze website gaat over Reinout</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti quasi ab et repellendus atque, quibusdam rem dolorem quas reprehenderit ipsum, amet vitae facere aliquam culpa possimus fugit dolore voluptas deserunt distinctio quam veritatis? Officiis minima, earum nobis cum nemo facilis repellat beatae dicta, similique fugiat voluptas consequatur distinctio, quasi fugit blanditiis in. Porro aut excepturi eaque sunt aliquid neque reprehenderit ad quis ipsam, distinctio dicta minus atque aperiam enim expedita nesciunt odio veniam odit rem sint. Sint ipsam ipsum aspernatur rem quae vero, eum magnam, sed beatae, eveniet corrupti quisquam adipisci doloribus nihil delectus facere quidem incidunt. Dolore, obcaecati nihil.</p>
+        </div>
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>
