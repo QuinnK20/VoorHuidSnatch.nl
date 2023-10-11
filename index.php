0b3669e14d9a36caff7f68fd4fb31aee3ab17fb6
DylanvSchagen <135001722+DylanvSchagen@users.noreply.github.com>
2023-10-11 09:49:37 +0200

    start your enginesss

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>hahahberkayheeftgeenvoorhuid.nl</title>
+</head>
+<body>
+    <header>
+        <a href="quinn.php">quinn</a>
+        <a href="jasper.php">jasper</a>
+        <a href="rick.php">rick</a>
+        <a href="reinout.php">reinout</a>
+        <a href="danilo.php">danilo</a>
+        <a href="dylan.php">dylan</a>
+    </header>
+    <main>
+
+    </main>
+    <footer>
+        
+    </footer>
+</body>
+</html>
