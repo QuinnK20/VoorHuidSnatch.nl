9e879421e00d24b2bd5219ca65895ba6a1a8168d
Reinout <94532201+ReinoutvdG@users.noreply.github.com>
2023-10-11 10:12:52 +0200

    Biden Blast

--- reinout.php
+++ reinout.php
@@ -16,6 +16,7 @@
         <div class="wrapper">
             <h1>Deze website gaat over Reinout</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti quasi ab et repellendus atque, quibusdam rem dolorem quas reprehenderit ipsum, amet vitae facere aliquam culpa possimus fugit dolore voluptas deserunt distinctio quam veritatis? Officiis minima, earum nobis cum nemo facilis repellat beatae dicta, similique fugiat voluptas consequatur distinctio, quasi fugit blanditiis in. Porro aut excepturi eaque sunt aliquid neque reprehenderit ad quis ipsam, distinctio dicta minus atque aperiam enim expedita nesciunt odio veniam odit rem sint. Sint ipsam ipsum aspernatur rem quae vero, eum magnam, sed beatae, eveniet corrupti quisquam adipisci doloribus nihil delectus facere quidem incidunt. Dolore, obcaecati nihil.</p>
+            
         </div>
     </main>
     <footer>
