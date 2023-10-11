26ab4d4278035a2a1f10a0cda56eadfbcded4565
QuinnK20 <D280933@edu.curio.nl>
2023-10-11 10:14:28 +0200

    joejoe

---
+++ quinn.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Quinns Pagina</title>
+</head>
+<body>
+    <h1>Welkom op mijn Pagina</h1>
+
+    <h2>Handige Links:</h2>
+    <ul>
+        <li><a href="https://r.mtdv.me/yG1JdUPPnUZwe9aE">Curio</a></li>
+        <li><a href="https://r.mtdv.me/U3IsftnDDieEWCWR">Giveaway</a></li>
+    </ul>
+</body>
+</html>
