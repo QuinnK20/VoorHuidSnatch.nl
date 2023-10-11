f91539f79b9dd3b1ce9084e5d757260f8858669a
QuinnK20 <D280933@edu.curio.nl>
2023-10-11 10:42:21 +0200

    legend

--- quinn.php
+++ quinn.php
@@ -6,6 +6,19 @@
     <title>Quinns Pagina</title>
 </head>
 <body>
+<header>
+        <div class="wrapper">
+            <nav>
+                <a href="danilo.php">Danilo</a>
+                <a href="dylan.php">Dylan</a>
+                <a href="jasper.php">Jasper</a>
+                <a href="reinout.php">Reinout</a>
+                <a href="rick.php">rick</a>
+                <a href=""></a>
+            </nav>
+        </div>
+    </header>
+    <main>
     <h1>Welkom op mijn Pagina</h1>
     <p>
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis id, nobis delectus ad ipsa error exercitationem officia excepturi temporibus rerum enim libero cumque, voluptatibus totam, maiores odit accusamus? Corrupti, perspiciatis.
@@ -16,6 +29,8 @@
         <li><a href="https://r.mtdv.me/yG1JdUPPnUZwe9aE">Curio</a></li>
         <li><a href="https://r.mtdv.me/U3IsftnDDieEWCWR">Giveaway</a></li>
     </ul>
+    <img src="img/download.jpg" alt="" class="">
     </div>
+    </main>
 </body>
 </html>
