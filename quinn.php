2c7bb741b0b98448f09aec32d40fb46f6c3b9a7e
QuinnK20 <D280933@edu.curio.nl>
2023-10-11 10:19:16 +0200

    update quinn

--- quinn.php
+++ quinn.php
@@ -5,11 +5,15 @@
 </head>
 <body>
     <h1>Welkom op mijn Pagina</h1>
+    <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis id, nobis delectus ad ipsa error exercitationem officia excepturi temporibus rerum enim libero cumque, voluptatibus totam, maiores odit accusamus? Corrupti, perspiciatis.
+    </p>
 
     <h2>Handige Links:</h2>
     <ul>
         <li><a href="https://r.mtdv.me/yG1JdUPPnUZwe9aE">Curio</a></li>
         <li><a href="https://r.mtdv.me/U3IsftnDDieEWCWR">Giveaway</a></li>
     </ul>
+    </div>
 </body>
 </html>
