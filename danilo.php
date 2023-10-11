54240a1e70fb908669590793e18cade2d41469bc
Daniloschmidt2003 <129153339+Daniloschmidt2003@users.noreply.github.com>
2023-10-11 10:00:58 +0200

    img map
    img map

---
+++ danilo.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>vooruit</title>
+</head>
+<header>
+<h1>dylan is niet groot</h1>
+
+</header>
+<body>
+    
+    
+</body>
+<footer>   
+    <p>hahaha dylan is klein </p>
+</footer>
+</html>
